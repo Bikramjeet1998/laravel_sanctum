aae3ba7bd5317ec5a684b0b45c0cc625c8c93127
Jaskaran <jaskaransingh4704@gmail.com>
2023-10-07 14:30:51 +0530

    Public keys loaded successfully and encrypted data using RSA

--- app/Http/Controllers/AsymmetricController.php
+++ app/Http/Controllers/AsymmetricController.php
@@ -1,48 +1,40 @@
 <?php
 
 namespace App\Http\Controllers;
-// include ('vendor/autoload.php');
 
-
-use Illuminate\Http\Request;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 use phpseclib3\Crypt\RSA;
 
 class AsymmetricController extends Controller
 {
-//     function encryptData($data)
-// {
-//     // Replace with the actual file path to your public key
-//     $publicKeyPath = 'storage/keys/public.pem';
+    /**
+     * Encrypts the given data using RSA encryption.
+     *
+     * @return Response The encrypted data as a response.
+     * @throws FileNotFoundException
+     */
+    public function encryptData()
+    {
+        $publicKeyPath = 'keys/public.pem';
+        if (Storage::disk('local')->exists($publicKeyPath)) {
 
-//     // Initialize the RSA instance and load the public key
-//     $rsa = new RSA();
-//     $rsa->loadKey(file_get_contents($publicKeyPath));
+            $fullPathOfPublicKey = Storage::disk('local')->get($publicKeyPath);
 
-//     // Encrypt the data
-//     $encryptedData = $rsa->encrypt($data);
+            $publicKey = RSA::load($fullPathOfPublicKey);
+            $data = 'This is the data to be encrypted.';
 
-//     return $encryptedData;
-// }
+            $ciphertext = $publicKey->encrypt($data);
 
+            // Return the encrypted data as a response
+            return response($ciphertext, 200)
+                ->header('Content-Type', 'application/x-pem-file');
 
-        public  function encryptData()
-        {
-            dd("hello");
-            $publicKeyPath ='storage/keys/public.pem';
-            dd(file_get_contents($publicKeyPath));
-            // Initialize the RSA instance
-            $rsa = new RSA();
-
-            // Load the public key
-            $rsa->loadPublicKey(file_get_contents($publicKeyPath));
-
-            //  Encrypt the data
-            //  $encryptedData = $rsa->encrypt($data);
-
-            //  return $encryptedData;
+        } else {
+            // Handle the case where the file doesn't exist
+            return response('File not found',  404);
         }
-
-
-
+    }
 
 }
